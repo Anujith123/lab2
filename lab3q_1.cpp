568e7dc2e11e78f7d3dcc84c8f125844296baee1
Anujith123 <42373652+Anujith123@users.noreply.github.com>
2018-08-16 16:03:30 +0000

    Add files via upload

---
+++ lab3q_1.cpp
@@ -0,0 +1,20 @@
+// library
+#include<iostream>
+// write the main function
+using namespace std;
+// write the main fuction
+int main(){
+// declaring variables
+int a=1;
+char b=2;
+float c=2.07;
+double d=2.01;
+bool e=0;
+//will get the size of the variable
+cout <<"size of char;"<<sizeof(char)<<"byte"<<" contents of b="<<b<<endl;
+cout <<"size of float;"<<sizeof(float)<<" contents of c="<<c<<endl;
+cout <<"size of int;"<<sizeof(int)<<"byte"<<" contents of a="<<a<<endl;
+cout <<"size of double;"<<sizeof(double)<<"byte"<<" contents of d="<<d<<endl;
+cout <<"size of bool;"<<sizeof(bool)<<"byte"<<" contents of e="<<e<<endl;
+return 0;
+}
